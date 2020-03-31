d648eed7cca6a2b91c546c519fc4f7e2a07b8364
Matt Vinall <mattv@office.q4inc.com>
2020-03-30 21:54:11 -0400

    setup files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+   (
+    <div className="App">
+      <header className="App-header">
+        <h1>COVID-19</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
