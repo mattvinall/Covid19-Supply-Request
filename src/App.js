f56585e06e0fc6f875aa04dec4b581eb50619888
Matt Vinall <mattv@office.q4inc.com>
2020-03-30 22:05:24 -0400

    set up file structure for react app and initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-   (
+   return (
     <div className="App">
       <header className="App-header">
         <h1>COVID-19</h1>
