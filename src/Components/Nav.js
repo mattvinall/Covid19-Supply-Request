a3e72b7b379bbebebd6fae34c1a10db85cbefe2f
MattVinall <matt.vinall7@gmail.com>
2020-04-01 02:00:44 -0400

    styling updates

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,19 +4,21 @@ import { Form, Icon, Header, Grid, Segment, Container } from 'semantic-ui-react'
 const Navigation = () => {
 	return (
 		<Container>
-			<Header as="h1" content="COVID-19 Supply" textAlign="center" />
-			<Grid>
-				<Grid.Row style={{ display: 'flex' }}>
-					<Grid.Column width="6">
-						<Form>
-							<Form.Input fluid label="search" placeholder="search items" />
-						</Form>
-					</Grid.Column>
-					<Grid.Column width="10" textAlign="right" style={{ margin: 'auto' }}>
-						<Icon name="plus" size="large" />
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
+			<Header as="h1" content="COVID-19 Supply" textAlign="center" color="white" />
+			<Segment>
+				<Grid>
+					<Grid.Row style={{ display: 'flex' }}>
+						<Grid.Column width="6">
+							<Form>
+								<Form.Input fluid label="search" placeholder="search items" />
+							</Form>
+						</Grid.Column>
+						<Grid.Column width="10" textAlign="right" style={{ margin: 'auto' }}>
+							<Icon name="plus" size="small" />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Segment>
 		</Container>
 	);
 };
