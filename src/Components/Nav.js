1dff9de66208f8238c2606e93b586c3a23d58ac5
MattVinall <matt.vinall7@gmail.com>
2020-04-01 01:42:33 -0400

    fixed style issue , created two column layout

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,18 +7,14 @@ const Navigation = () => {
 			<Header as="h1" content="COVID-19 Supply" textAlign="center" />
 			<Grid>
 				<Grid.Row style={{ display: 'flex' }}>
-					<Segment>
-						<Grid.Column width="6">
-							<Form>
-								<Form.Input fluid label="search" placeholder="search items" />
-							</Form>
-						</Grid.Column>
-					</Segment>
-					<Segment>
-						<Grid.Column width="10" textAlign="right" style={{ margin: 'auto' }}>
-							<Icon name="plus" size="large" />
-						</Grid.Column>
-					</Segment>
+					<Grid.Column width="6">
+						<Form>
+							<Form.Input fluid label="search" placeholder="search items" />
+						</Form>
+					</Grid.Column>
+					<Grid.Column width="10" textAlign="right" style={{ margin: 'auto' }}>
+						<Icon name="plus" size="large" />
+					</Grid.Column>
 				</Grid.Row>
 			</Grid>
 		</Container>
