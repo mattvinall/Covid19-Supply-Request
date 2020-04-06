bdb22ccd07b3bbdcbd7612e34eb3832a8a7e4919
Yash Kothari <yashk.23@gmail.com>
2020-04-05 20:11:03 -0400

    Fixed event handlers and their bindings

--- src/Components/AddSupplyForm.js
+++ src/Components/AddSupplyForm.js
@@ -16,58 +16,102 @@ const options = [
   { key: 'v', text: 'Ventilators', value: 'ventilators' },
   { key: 'vfil', text: 'Filters for Ventilators', value: 'filters' },
   { key: 'sc', text: 'Shoe Covering', value: 'shoe_covering' },
+  { key: 'cs', text: 'Cleaning Supplies (e.g. bleach)', value: 'cleaning_supplies' },
 ]
 
 class AddSupplyForm extends Component {
-  state = {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRequest: true,
+      supplyType: '',
+      quantity: 0,
+      description: '',
+      firstName:'',
+      lastName:'',
+      organization:'',
+      department:'',
+      phone:'',
+      email:'',
+    };
 
-  handleChange = (e, { value }) => this.setState({ value })
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    // const target = e.target
+    // const name = target.name
+    // const value = null
+    // switch (name) {
+    //   case 'isRequest':
+    //     value = target.checked
+    //     if (value == true) {
+
+    //     } else {
+
+    //     }
+    //     break;
+      // default:
+      //  const value = target.value
+    // } 
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
 
   render() {
-    const { value } = this.state
+    // const { value } = this.state
     return (
-      <Form>
-        <Form.Group inline required='true'>
+      <Form onSubmit={this.handleSubmit}>
+        {/* <Form.Group inline name='isRequest' required='true'>
           <label>I am</label>
           <Form.Radio
             label='Requesting'
-            value='req'
-            checked='true' //{value === 'sm'}
+            value='true'
+            checked={value === 'true'}
             onChange={this.handleChange}
           />
           <Form.Radio
             label='Offering'
-            value='off'
-            checked={value === 'md'}
+            value='false'
+            checked={value === 'false'}
             onChange={this.handleChange}
             disabled='true'
           />
-        </Form.Group>
+        </Form.Group> */}
         <Form.Group widths='equal'>
           <Form.Select
             fluid
             label='Supply Type'
+            name='supplyType'
             options={options}
             placeholder='— Select one'
             required='true'
+            onChange={this.handleChange}
           />
-          <Form.Input fluid label='Quantity' placeholder='Quantity'/>
+          <Form.Input fluid label='Quantity' name='quantity' placeholder='Quantity' type='number' min="1" onChange={this.handleChange}/>
         </Form.Group>
-        <Form.TextArea label='Description' placeholder='Tell us anything else about your request/offer. For example, brand/model, delivery instructions, etc.' />
+        <Form.TextArea label='Description' name='description' placeholder='Tell us anything else about your request/offer. For example, brand/model, delivery instructions, etc.' onChange={this.handleChange}/>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='First Name' placeholder='First Name' required='true'/>
-          <Form.Input fluid label='Last Name' placeholder='Last Name' required='true'/>
-        </Form.Group>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='Organization Name' placeholder='Organization Name' />
-          <Form.Input fluid label='Department Name' placeholder='Department Name' />
+          <Form.Input fluid label='First Name' name='firstName' placeholder='First Name' required='true' onChange={this.handleChange}/>
+          <Form.Input fluid label='Last Name' name='lastName' placeholder='Last Name' required='true' onChange={this.handleChange}/>
         </Form.Group>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='Phone Number' placeholder='Phone Number' />
-          <Form.Input fluid label='Email Address' placeholder='Email Address' />
+          <Form.Input fluid label='Organization Name' name='organization' placeholder='Organization Name' onChange={this.handleChange}/>
+          <Form.Input fluid label='Department Name' name='department' placeholder='Department Name' onChange={this.handleChange}/>
         </Form.Group>
-        <Form.Checkbox label='I agree to the Terms and Conditions' /> 
-        <Form.Button>Submit</Form.Button>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='Phone Number' name='phone' placeholder='Phone Number' type='tel' required='true' onChange={this.handleChange}/>
+          <Form.Input fluid label='Email Address' name='email' placeholder='Email Address' type='email' required='true' onChange={this.handleChange}/>
+        </Form.Group>
+        <Form.Checkbox label='I agree to the Terms and Conditions' required='true' onChange={this.handleChange}/> 
+        <Form.Button onClick="this.handleClick">Submit</Form.Button>
       </Form>
     )
   }
