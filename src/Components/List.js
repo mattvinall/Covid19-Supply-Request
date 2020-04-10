808a35111465bbf57c7354106f819180062264e6
Rosie Bahrani <rosie.bahrani@uwaterloo.ca>
2020-04-10 13:58:36 -0400

    corrected the UI so that we do not have multiple entries from both the list and the details section; now we only have multiple entries for the list -- which is vretical, and just a single entry for the details

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Container, Fragment, useEffect } from 'react';
 import { Item } from 'semantic-ui-react';
 import './List.css';
 
@@ -7,26 +7,24 @@ const List = (props) => {
 		props.sendData('hello');
 	});
 	return (
-		<Fragment>
-			{props.data.map((item, index) => {
-				return (
-					<Item.Group key={index} className="col-lg-4">
-						<Item className={'item-border border-right'}>
-							<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-							<Item.Content>
-								<Item.Header as="a">{item.organization}</Item.Header>
-								<Item.Meta>{item.department}</Item.Meta>
-								<Item.Description>
-									<Item.Meta>{item.item}</Item.Meta>
-									<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-								</Item.Description>
-								<Item.Extra as="a">Additional Details</Item.Extra>
-							</Item.Content>
-						</Item>
-					</Item.Group>
-				);
-			})}
-		</Fragment>
+		<Item.Group className="col-lg-4">
+		{props.data.map((item, index) => {
+			return (
+				<Item className={'item-border border-right'}>
+					<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
+						<Item.Content>
+							<Item.Header as="a">{item.organization}</Item.Header>
+							<Item.Meta>{item.department}</Item.Meta>
+							<Item.Description>
+								<Item.Meta>{item.item}</Item.Meta>
+								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
+							</Item.Description>
+							<Item.Extra as="a">More info</Item.Extra>
+						</Item.Content>
+					</Item>
+			);
+		})},
+		</Item.Group>
 	);
 };
 
