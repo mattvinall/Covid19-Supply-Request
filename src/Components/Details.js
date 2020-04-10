a752e718907bb538cb4858ebed9868aebda41467
Rosie Bahrani <rosie.bahrani@uwaterloo.ca>
2020-04-10 13:36:33 -0400

    fixing the details to be an individual display (instead of a mutiplied display or each element in the Firebase database

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { Fragment, useEffect, List } from 'react';
 import './Details.css';
 import { Grid, Item } from 'semantic-ui-react';
 
 const Details = (props) => {
 	{
-		return props.data.map((item, index) => {
 			return (
-				<Grid celled key={index} centered={false}>
+				<Grid celled centered={false}>
 					<Grid.Column width={13}>
-						<Item.Content>
 							<h3>SunnyBrooke</h3>
 							<h4>Department:</h4>
-							<Item.Meta>{item.department}</Item.Meta>
 							<h4>Requesting:</h4>
-							<Item.Description>
-								<Item.Meta>{item.item} x {item.quantity}</Item.Meta>
-							</Item.Description>
 							<h4>Address:</h4>
 							<p>2075 Bayview Ave, Toronto, ON M4N 3M5</p>
 							<h4>Email:</h4>
@@ -24,12 +19,10 @@ const Details = (props) => {
 							<p>(416) 480-6100</p>
 							<h4>Special instructions:</h4>
 							<p>Please drop off all the donations on the 3rd floor.</p>
-						</Item.Content>
 					</Grid.Column>
 				</Grid>
 			);
-		});
-	}
+		};
 };
 
 export default Details;
--- src/Components/List.js
+++ src/Components/List.js
@@ -3,9 +3,9 @@ import { Item } from 'semantic-ui-react';
 import './List.css';
 
 const List = (props) => {
-	// useEffect(() => {
-	// 	props.sendData('hello');
-	// });
+	useEffect(() => {
+		props.sendData('hello');
+	});
 	return (
 		<Fragment>
 			{props.data.map((item, index) => {
@@ -14,7 +14,6 @@ const List = (props) => {
 						<Item className={'item-border border-right'}>
 							<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
 							<Item.Content>
-								<Item.Meta as="h5">{item.isRequest ? 'Requesting' : 'Offering'}</Item.Meta>
 								<Item.Header as="a">{item.organization}</Item.Header>
 								<Item.Meta>{item.department}</Item.Meta>
 								<Item.Description>
@@ -24,66 +23,6 @@ const List = (props) => {
 								<Item.Extra as="a">Additional Details</Item.Extra>
 							</Item.Content>
 						</Item>
-						{/* <Item className={'item-border border-right'}>
-						<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">Masks</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item>
-					<Item className={'item-border border-right'}>
-						<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">{item.organization}</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item>
-					<Item className={'item-border border-right'}>
-						<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">{item.organization}</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item>
-					<Item className={'item-border border-right'}>
-						<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">{item.organization}</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item>
-					<Item className={'item-border border-right'}>
-						<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">{item.organization}</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item> */}
 					</Item.Group>
 				);
 			})}
