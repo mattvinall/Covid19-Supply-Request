d3eb0cdce1e9ead16c02555a6005a69d3ecf3e62
MattVinall <matt.vinall7@gmail.com>
2020-04-11 12:43:06 -0400

    updated phone and email text to be links and dynamic from the form submission

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,13 +1,8 @@
-import React, { Container, Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { Item } from 'semantic-ui-react';
-import './List.css';
 
 const List = (props) => {
-	useEffect(() => {
-		props.sendData('hello');
-	});
 	return (
-<<<<<<< HEAD
 		<Fragment>
 			{props.data.map((item, index) => {
 				return (
@@ -27,26 +22,6 @@ const List = (props) => {
 				);
 			})}
 		</Fragment>
-=======
-		<Item.Group className="col-lg-4">
-		{props.data.map((item, index) => {
-			return (
-				<Item className={'item-border border-right'}>
-					<Item.Image size="tiny" src="https://react.semantic-ui.com/images/wireframe/image.png" />
-						<Item.Content>
-							<Item.Header as="a">{item.organization}</Item.Header>
-							<Item.Meta>{item.department}</Item.Meta>
-							<Item.Description>
-								<Item.Meta>{item.item}</Item.Meta>
-								<Item.Meta>Quantity x {item.quantity}</Item.Meta>
-							</Item.Description>
-							<Item.Extra as="a">More info</Item.Extra>
-						</Item.Content>
-					</Item>
-			);
-		})},
-		</Item.Group>
->>>>>>> 808a35111465bbf57c7354106f819180062264e6
 	);
 };
 
