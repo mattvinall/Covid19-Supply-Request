d8f27055a1d882d7f9d0d1ddec0ca0932af4a99a
Yash Kothari <yashk.23@gmail.com>
2020-04-16 22:50:07 -0400

    Changed the terms and conditions

--- src/Components/FormModal.js
+++ src/Components/FormModal.js
@@ -192,17 +192,17 @@ class FormModal extends Component {
 							/>
 						</Form.Group>
 						<Form.Checkbox
-							label="I agree to the Terms and Conditions"
+							label="I accept full liability for the quality and safety of this product and acknowledge that Covid Care Package does not guarantee the accuracy or completeness of any postings."
 							required
 							onChange={this.handleTerms}
 						/>
-						<Item.Content>
+						{/* <Item.Content>
 							<Item.Description
 								as="p"
 								content="I accept full liability for the quality and safety of this product and acknowledge that Covid Care Package does not guarantee the accuracy or completeness of any postings."
 								style={{ margin: '15px 0' }}
 							/>
-						</Item.Content>
+						</Item.Content> */}
 
 						<Modal.Actions>
 							<Form.Button open={false} primary type="submit">
