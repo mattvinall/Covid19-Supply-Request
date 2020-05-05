71f11dbc2b57d5049e46df1b79bfa1706675b761
Bhaag Marway <bsmarway@gmail.com>
2020-05-05 19:39:44 -0400

    added columns

--- src/Components/List.js
+++ src/Components/List.js
@@ -5,41 +5,58 @@ const List = (props) => {
 	const { data, searchTerm } = props;
 	return (
 		<Item.Group className="ui grid">
-				<Item.Content className={'row item-border border-right'}>
-					<Item.Header as="h3" className="four wide column">Supply Type</Item.Header>
-					<Item.Description className="six wide column">
-						<Item.Meta as="h3" >Organization</Item.Meta>
-					</Item.Description >
-					<Item.Description className="four wide column">
-						<Item.Meta as="h3">Quantity</Item.Meta>
-					</Item.Description >
-					<div className="two wide column">
-						
-					</div>
-				</Item.Content>
-				{data.filter((data) => {
-					const searchRegExp = new RegExp(searchTerm, "i")
-					return searchRegExp.test(data.supplyType)
-				}).map((item, index) => {
-					return (
-						<Item.Content key={index} className={'row item-border border-right'}>
-							<Item.Header as="h5" className="four wide column">{item.supplyType}</Item.Header>
-							<Item.Description className="six wide column">
-								<Item.Meta>{item.organization}</Item.Meta>
-							</Item.Description >
-							<Item.Description className="four wide column">
-								<Item.Meta>{item.quantity}</Item.Meta>
-							</Item.Description >
-							<div className="one wide column" onClick={() => props.selectItem(item)} style={{ cursor: 'pointer' }}>
-								<Icon color="blue" name='edit' />
-							</div>
-							<div className="one wide column" onClick={() => props.selectItem(item)} style={{ cursor: 'pointer' }}>
-								<Icon color="red" name='delete' />
-							</div>
-						</Item.Content>
-					);
-				})}
-			</Item.Group>
+			<Item.Content className={'row item-border border-right'}>
+				<Item.Header as="h3" className="two wide column">Category</Item.Header>
+				<Item.Header as="h3" className="two wide column">Name</Item.Header>
+				<Item.Header as="h3" className="one wide column">Status</Item.Header>
+				<Item.Header as="h3" className="one wide column">QTY</Item.Header>
+				<Item.Header as="h3" className="two wide column">Description</Item.Header>
+				<Item.Header as="h3" className="two wide column">Key Documents</Item.Header>
+				<Item.Header as="h3" className="two wide column">Expected Delivery</Item.Header>
+				<Item.Header as="h3" className="two wide column">Contact</Item.Header>
+				<Item.Header as="h3" className="two wide column">Files</Item.Header>
+			</Item.Content>
+			{data.filter((data) => {
+				const searchRegExp = new RegExp(searchTerm, "i")
+				return searchRegExp.test(data.supplyType)
+			}).map((item, index) => {
+				return (
+					<Item.Content key={index} className={'row item-border border-right'}>
+						<Item.Header as="h5" className="two wide column">{item.supplyType}</Item.Header>
+						<Item.Description className="two wide column">
+							<Item.Meta>Excel Gum</Item.Meta>
+						</Item.Description >
+						<Item.Description className="one wide column">
+							<Item.Meta>On the way</Item.Meta>
+						</Item.Description >
+						<Item.Description className="one wide column">
+							<Item.Meta>20</Item.Meta>
+						</Item.Description >
+						<Item.Description className="two wide column">
+							<Item.Meta>Freshens your breath</Item.Meta>
+						</Item.Description >
+						<Item.Description className="two wide column">
+							<Item.Meta>Receipt.pdf</Item.Meta>
+						</Item.Description >
+						<Item.Description className="two wide column">
+							<Item.Meta>1/10/2020 12:30AM</Item.Meta>
+						</Item.Description >
+						<Item.Description className="two wide column">
+							<Item.Meta>Joanna Waters</Item.Meta>
+						</Item.Description >
+						<Item.Description className="two wide column">
+							<Item.Meta>Receipt.jpg</Item.Meta>
+						</Item.Description >
+						<div className="one wide column" onClick={() => props.selectItem(item)} style={{ cursor: 'pointer' }}>
+							<Icon color="blue" name='edit' />
+						</div>
+						<div className="one wide column" onClick={() => props.selectItem(item)} style={{ cursor: 'pointer' }}>
+							<Icon color="red" name='delete' />
+						</div>
+					</Item.Content>
+				);
+			})}
+		</Item.Group>
 	);
 };
 
