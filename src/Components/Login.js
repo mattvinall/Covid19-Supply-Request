d0b411a133163c8ecbe53bc1614a70cdaadeaecf
Matt <matt.vinall7@gmail.com>
2020-05-05 21:47:48 -0400

    updates to login component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -22,6 +22,8 @@ const particleStyle = {
 	}
 };
 
+const db = firebase.firestore();
+
 var actionCodeSettings = {
 	// URL you want to redirect back to. The domain (www.example.com) for this
 	// URL must be whitelisted in the Firebase Console.
@@ -46,21 +48,36 @@ class Login extends Component {
 		this.signIn();
 	}
 
+	validateEmail = async (email) => {
+		let domain = email.split('@').pop();
+		console.log(domain);
+		await db.collection('hospitals').doc(domain).get().then((snapshot) => {
+			const result = snapshot.exists ? true : false;
+			console.log(result);
+			return result;
+		});
+	};
+
 	// send sign in link
-	sendLoginLink = (email, actionCodeSettings) => {
-		firebase
-			.auth()
-			.sendSignInLinkToEmail(email, actionCodeSettings)
-			.then(function() {
-				// The link was successfully sent. Inform the user.
-				// Save the email locally so you don't need to ask the user for it again
-				// if they open the link on the same device.
-				window.localStorage.setItem('emailForSignIn', email);
-			})
-			.catch(function(error) {
-				// Some error occurred, you can inspect the code: error.code
-				console.log('send login link error', error.code);
-			});
+	sendLoginLink = async (email, actionCodeSettings) => {
+		if (await this.validateEmail(email)) {
+			console.log('validated');
+			firebase
+				.auth()
+				.sendSignInLinkToEmail(email, actionCodeSettings)
+				.then(function() {
+					// The link was successfully sent. Inform the user.
+					// Save the email locally so you don't need to ask the user for it again
+					// if they open the link on the same device.
+					window.localStorage.setItem('emailForSignIn', email);
+				})
+				.catch(function(error) {
+					// Some error occurred, you can inspect the code: error.code
+					console.log('send login link error', error.code);
+				});
+		} else {
+			console.log('thou shall not pass');
+		}
 	};
 
 	signIn = () => {
